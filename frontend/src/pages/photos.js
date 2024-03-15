cd04a06d68d09e3f8f22ad06c2ff716fa75fcd3a
JoshuaJ18 <109106928+JoshuaJ18@users.noreply.github.com>
2024-03-15 12:09:07 -0400

    Temporary implementation of the image carousel in the photos section of the website

--- frontend/src/pages/photos.js
+++ frontend/src/pages/photos.js
@@ -1,10 +1,35 @@
 import './../pages_css/photos.css'; 
+import { photosData } from "./photosDataList.js";
+import { useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 function Photos() {
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    setSlide(slide === photosData.length - 1 ? 0 : slide + 1);
+  };
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? photosData.length - 1 : slide - 1);
+  };
+
   return (
     <div className="photo-container">
+      <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
       <div className="photos">
-        <h1>Image placeholder</h1>
+        <>
+          <div className="photo_1">
+            <img src={photosData[(slide) % (photosData.length)]} alt="1" />
+          </div>
+          <div className="photo_2">
+            <img src={photosData[(slide+1) % (photosData.length)]} alt="2" />
+          </div>
+          <div className="photo_3">
+            <img src={photosData[(slide+2) % (photosData.length)]} alt="3" />
+          </div>
+        </>
+        <BsArrowRightCircleFill onClick={nextSlide} className="arrow arrow-right" />
       </div>
     </div>
   );
---
+++ frontend/src/pages/photosDataList.js
@@ -0,0 +1,11 @@
+import placeholder1 from "./../media_files/images/placeholder1.jpg";
+import placeholder2 from "./../media_files/images/placeholder2.png";
+import placeholder3 from "./../media_files/images/placeholder3.jpg";
+import placeholder4 from "./../media_files/images/placeholder4.png";
+
+export const photosData = [
+    placeholder1,
+    placeholder2,
+    placeholder3,
+    placeholder4,
+]
